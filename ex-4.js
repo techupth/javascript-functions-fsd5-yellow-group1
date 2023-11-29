b17ceb1e7890aee7a9e7f49574a7b72b773ca749
peerawet <peerawet1996@gmail.com>
2023-11-29 12:35:33 +0700

    finish ex1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,17 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => a + b,
+
+  subtract: (a, b) => a - b,
+
+  multiply: (a, b) => a * b,
+
+  divide: (a, b) => a / b,
+};
+let a = 10;
+let b = 20;
+let addResult = calculator.add(a, b);
+console.log(addResult);
+
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
